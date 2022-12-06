1b40ddc18a5e86011e4d86784ba2ef5213793388
Anslem1 <andrewanslem1@gmail.com>
2022-12-06 07:39:50 +0100

    removed a /  in urlConfig

--- src/Redux/helpers/urlConfig.js
+++ src/Redux/helpers/urlConfig.js
@@ -1,5 +1,5 @@
 const baseURL =
   window.location.hostname === 'localhost'
     ? 'http://localhost:5000'
-    : 'https://elimart-serverr.onrender.com/'
+    : 'https://elimart-serverr.onrender.com'
 export const API = `${baseURL}/api`
